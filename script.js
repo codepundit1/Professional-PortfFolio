a7159a1f98b1f02bfe8d0f26218b28cccd853734
codepundit1 <jahid35-266@diu.edu.bd>
2020-11-05 15:35:49 +0600

    complete my service and start my skill

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
-           $('header nav').css('background','blue');
+           $('header nav').css('background','#00539C');
         }else{
             $('header nav').css('background','transparent');
         }
